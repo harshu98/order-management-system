ccea4665bc4596e14e7547e3a37049f54247d26f
wocero2582@sceath.com <wocero2582@sceath.com>
2022-07-16 21:29:57 +0530

    final

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { map } from 'rxjs';
+import { environment } from '../environments/environment.prod';
+
+const Endpoint = environment.ApiEndpoint;
 interface users {
   username: string;
   password: string;
@@ -22,10 +24,27 @@ export class AppService {
   constructor(public http: HttpClient) { }
 
   checkUser(): Observable<users[]> {
-    return this.http.get<users[]>('../assets/users.json');
+    console.log(Endpoint);
+    return this.http.get<users[]>(Endpoint + '/login');
   };
 
   getCustomerDetails(): Observable<customers[]> {
-    return this.http.get<customers[]>('../assets/sample.json');
+    return this.http.get<customers[]>(Endpoint + '/getCustomers');
+  }
+
+  addOrder(data: customers): Observable<boolean> {
+    console.log(data);
+    const params = data;
+    return this.http.post<boolean>(Endpoint + '/addOrder', params);
+  }
+
+  editOrder(data: customers): Observable<boolean> {
+    const params = data;
+    return this.http.put<boolean>(Endpoint + '/editOrder', params);
+  }
+  
+  deleteOrder(id: number): Observable<boolean> {
+    const params = { "id": id };
+    return this.http.post<boolean>(Endpoint + '/deleteOrder', params);
   }
 }
--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -3,13 +3,7 @@ import { lastValueFrom } from 'rxjs';
 import { AppService } from '../app.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-interface Country {
-  id?: number;
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
+
 interface customers {
   "Order_Number": number,
   "Order_Due_Date": string,
@@ -27,8 +21,10 @@ export class OrderPageComponent implements OnInit {
 
   constructor(private service: AppService, private fb: FormBuilder, private modalService: NgbModal) { }
   page = 1;
-  isAdd=false;
-  isEdit=true;
+  isAdd = false;
+  isEdit = true;
+  show = false;
+  Message = '';
   pageSize = 4;
   orders: customers[] = [];
   submitted = false;
@@ -40,21 +36,23 @@ export class OrderPageComponent implements OnInit {
     console.log(this.orders);
     this.collectionSize = this.orders.length;
     this.form = this.fb.group({
-      orderNumber: ['', Validators.required],
+      orderNumber: [''],
       dueDate: ['', Validators.required],
       customerName: ['', Validators.required],
       customerAddress: ['', Validators.required],
-      customerPhone: ['', Validators.required],
-      orderTotal: ['', Validators.required],
-    })
+      customerPhone: ['', [Validators.required, Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+      orderTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+    });
+    this.form.controls['orderNumber'].disable()
   }
   get f() { return this.form.controls; }
   addButton(content: any) {
+    this.form.reset();
     this.modalService.open(content, { centered: true });
-    this.isAdd=true;
-    this.isEdit=false;
+    this.isAdd = true;
+    this.isEdit = false;
   }
-  addOrder() {
+  async addOrder() {
     console.log('add order');
     console.log(this.form);
     this.submitted = true;
@@ -62,33 +60,87 @@ export class OrderPageComponent implements OnInit {
       console.log('"INVALID"')
       return;
     }
+    console.log(this.form.value);
+    const formData: customers = {
+      "Order_Number": this.orders.length + 1,
+      "Order_Due_Date": this.form.value.dueDate,
+      "Customer_name": this.form.value.customerName,
+      "Customer_Address": this.form.value.customerAddress,
+      "Customer_Phone": this.form.value.customerPhone,
+      "OrderTotal": this.form.value.orderTotal
+    }
+    const result = await lastValueFrom(this.service.addOrder(formData));
+    if (result) {
+      this.modalService.dismissAll();
+      this.Message = "Order added successfully";
+      this.show = true;
+      this.orders = await lastValueFrom(this.service.getCustomerDetails());
+    }
+    else {
+      this.Message = "Order failed to be added";
+      this.show = true;
+    }
   }
   editButton(id: number, content: any) {
     console.log('edit order');
-    this.isAdd=false;
-    this.isEdit=true;
+    this.isAdd = false;
+    this.isEdit = true;
     this.modalService.open(content, { centered: true });
-    console.log(id);
     const editableitem = this.orders.find(order => order.Order_Number === id);
     if (editableitem != null) {
       this.form.setValue(
-        { 
+        {
           orderNumber: editableitem.Order_Number,
-          dueDate: editableitem.Order_Due_Date, 
-          customerName: editableitem.Customer_name, 
-          customerAddress: editableitem.Customer_Address, 
-          customerPhone: editableitem.Customer_Phone, 
-          orderTotal: editableitem.OrderTotal 
+          dueDate: editableitem.Order_Due_Date,
+          customerName: editableitem.Customer_name,
+          customerAddress: editableitem.Customer_Address,
+          customerPhone: editableitem.Customer_Phone,
+          orderTotal: editableitem.OrderTotal
         }
-        );
+      );
     }
   }
-  editOrder(){
+  async editOrder() {
     console.log("editOrder");
+    this.submitted = true;
+    if (this.form.invalid) {
+      console.log('"INVALID"')
+      return;
+    }
+    const formData: customers = {
+      "Order_Number": this.f['orderNumber'].value,
+      "Order_Due_Date": this.form.value.dueDate,
+      "Customer_name": this.form.value.customerName,
+      "Customer_Address": this.form.value.customerAddress,
+      "Customer_Phone": this.form.value.customerPhone,
+      "OrderTotal": this.form.value.orderTotal
+    }
+    const result = await lastValueFrom(this.service.editOrder(formData));
+    if (result) {
+      this.modalService.dismissAll();
+      this.Message = "Order Updated successfully";
+      this.show = true;
+      this.orders = await lastValueFrom(this.service.getCustomerDetails());
+    }
+    else {
+      this.Message = "Order failed to Update";
+      this.show = true;
+    }
   }
-  deleteOrder(id: number) {
+  async deleteOrder(id: number) {
     console.log('delete order');
     console.log(id);
+    const result = await lastValueFrom(this.service.deleteOrder(id));
+    if (result) {
+      this.modalService.dismissAll();
+      this.Message = "Order Deleted successfully";
+      this.show = true;
+      this.orders = await lastValueFrom(this.service.getCustomerDetails());
+    }
+    else {
+      this.Message = "Order failed to delete";
+      this.show = true;
+    }
   }
   refreshCountries() { }
 
